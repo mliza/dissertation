9a5c772489eb544715331e1cb6b099223b417033
Martin E. Liza <mliza@email.arizona.edu>
2024-09-15 23:08:08 -0400

    Added compile.sh

---
+++ compile.sh
@@ -0,0 +1,10 @@
+# This function compiles the thesis
+compileLatex() {
+    pdflatex $1.tex;
+    makeindex $1.nlo -s nomencl.ist -o $1.nls;
+    biber $1;
+    pdflatex $1.tex; 
+    rm $1.aux $1.bbl $1.bcf $1.blg $1.glo* $1.gls* $1.ist $1.log $1.out $1.run.xml $1.slg $1.slo $1.sls $1.tdo $1.toc $1.glg* $1.lot $1.lof $1.dvi;
+}
+
+compileLatex main
