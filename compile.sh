812ff409a1002c2ffc7f987deeabee2f8318753f
Martin E. Liza <mliza@email.arizona.edu>
2024-09-20 17:26:06 -0400

    Added submodule check

--- compile.sh
+++ compile.sh
@@ -1,4 +1,14 @@
-# This function compiles the thesis
+# Auxiliary function to handle submodules
+gitSubmodule() {
+    if [ git submodule status | grep --quiet '^-' ]; then
+        git submodule update --init --recursive
+    else
+        git submodule update --remote configureation/myResources
+    fi
+}
+
+
+# Auxiliary function used to compile dissertation
 compileLatex() {
     pdflatex $1.tex;
     makeindex $1.nlo -s nomencl.ist -o $1.nls;
@@ -7,4 +17,5 @@ compileLatex() {
     rm $1.aux $1.bbl $1.bcf $1.blg $1.glo* $1.gls* $1.ist $1.log $1.out $1.run.xml $1.slg $1.slo $1.sls $1.tdo $1.toc $1.glg* $1.lot $1.lof $1.dvi;
 }
 
+gitSubmodule
 compileLatex main
