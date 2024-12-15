262fe8ef41a23bbb34bf7b75b8a43b418f465a6e
Martin E. Liza <mliza@email.arizona.edu>
2024-12-15 12:03:56 -0500

    updated chapter4

--- codes/chapter4.py
+++ codes/chapter4.py
@@ -1,11 +1,15 @@
 from haot import aerodynamics
 from haot import optics
+from haot import quantum_mechanics
 from haot import constants
 from haot import conversions
 from ambiance import Atmosphere
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator 
+from matplotlib.patches import Patch
+from mpl_toolkits.mplot3d import Axes3D
 
 
 import IPython
@@ -45,13 +49,13 @@ def test_aerodynamics_module(fig_config='None', output_png='None'):
                           fontsize=fig_config["axis_label_size"])
         axs[1].legend()
         
-        if output_png: 
+        if output_png is not None: 
             plt.tight_layout()
             plt.savefig(os.path.join(output_png,'aerodynamics.png'), format='png',
                 bbox_inches='tight', dpi=fig_config["dpi_size"])
             plt.close()
 
-        if not output_png:
+        if output_png is None:
             plt.show()
 
 def test_optics_module(fig_config='None', output_png='None'):
@@ -82,12 +86,95 @@ def test_optics_module(fig_config='None', output_png='None'):
         plt.legend(fontsize=fig_config['legend_size'])
         plt.xticks(fontsize=fig_config['ticks_size'])
         plt.yticks(fontsize=fig_config['ticks_size'])
-        plt.savefig(os.path.join(output_png,
+
+        if output_png is not None:
+            plt.savefig(os.path.join(output_png,
             f'kerlPolarizability_{wavelength_nm}nm.png'), format = 'png',
                     bbox_inches='tight', dpi=fig_config['dpi_size'])
-        plt.close()
+            plt.close()
+        if output_png is None:
+            plt.show()
+
+def test_quantum_mechanics_module(fig_config=None, output_png=None):
+    vibrational_number = 1 
+    rotational_number = 40
+    temperature_K = [100.0, 500.0, 1000.0]
+    molecule = ['N2', 'O2', 'NO']
+    elev_ang = 16
+    azim_ang = 40
+
+    for k in molecule:
+        # [temperature, vibrational, rotational]
+        distribution = np.zeros([np.size(temperature_K),
+                                 vibrational_number + 1,
+                                 rotational_number + 1])
+        for t, val in enumerate(temperature_K):
+            distribution[t] = quantum_mechanics.boltzmann_distribution( 
+                                                val, k, 
+                                                vibrational_number,
+                                                rotational_number, False)
+            if fig_config:
+                # Plots Distribution function
+                vib_axis = range(vibrational_number + 1)
+                rot_axis = range(rotational_number + 1)
+                vib_mesh, rot_mesh = np.meshgrid(vib_axis, rot_axis)
+                fig, ax = plt.subplots(subplot_kw={"projection": "3d"},
+                                       figsize=(fig_config['fig_width'],
+                                                fig_config['fig_height']))
+                # Color options 
+                colormaps = ['viridis', 'plasma', 'cividis']
+
+                # Plot tempeteratures
+                for i, temp in enumerate(temperature_K):
+                    cmap = plt.get_cmap(colormaps[i])
+                    surf = ax.plot_surface(vib_mesh.T, rot_mesh.T, 
+                                           distribution[i],
+                                           cmap=cmap, edgecolor='k',
+                                           alpha=0.8,
+                                           linewidth=0.5)
+                    ax.text(x=vibrational_number // 2, 
+                            y=rotational_number // (4 - i),
+                            z=np.max(distribution[i]),
+                            s=f'T = {temp} [K]', color='k', 
+                            fontsize=fig_config['legend_size'])
+
+                # Change grid and background colors
+                ax.set_facecolor('white') 
+                fig.patch.set_facecolor('white')
+                ax.grid(False)
+                ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0)) 
+                ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+                ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+
+                # 3D axis views
+                ax.view_init(elev=elev_ang, azim=azim_ang)
+
+                # Labels
+                ax.set_xlabel('Vibrational # $[\;]$',
+                               fontsize=fig_config['axis_label_size'])
+
+                ax.set_ylabel('Rotational # $[\;]$',
+                               fontsize=fig_config['axis_label_size'])
+
+                ax.set_zlabel('Boltzmann Distribution $[\;]$',
+                               fontsize=fig_config['axis_label_size'])
+
+                # Integers for axis ticks
+                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
 
+                if output_png:
+                    plt.savefig(os.path.join(output_png,
+                                f'boltzmannDistribution_{k}.png'), 
+                                format = 'png',
+                            bbox_inches='tight', 
+                            pad_inches=0.3,
+                            dpi=fig_config['dpi_size'])
+                    plt.close()
+                else:
+                    plt.show()
+            
 
 if __name__ == "__main__":
     fig_config = {}
@@ -105,6 +192,8 @@ if __name__ == "__main__":
     test_aerodynamics_module(fig_config, output_png)
 
     test_optics_module(fig_config, output_png)
+
+    test_quantum_mechanics_module(fig_config, output_png) 
 
 
 
