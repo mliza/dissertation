745f05a9a2702f6189cb6e92659e51d7fbd6c0de
Martin E. Liza <mliza@email.arizona.edu>
2024-12-15 14:05:30 -0500

    Added additional figures for chapter 4

--- codes/chapter4.py
+++ codes/chapter4.py
@@ -58,42 +58,108 @@ def test_aerodynamics_module(fig_config='None', output_png='None'):
         if output_png is None:
             plt.show()
 
-def test_optics_module(fig_config='None', output_png='None'):
-    keys = ['N2', 'O2', 'Air']
+def test_optics_module(fig_config=None, output_png=None):
+    keys = ['N2', 'O2', 'H2', 'Air']
     wavelength_nm = 633
-    temperature_K = np.linspace(200, 1500, 200) 
+    temperature_K = np.arange(100, 1500 + 50, 50)
     dict_kerl = { }
+    dict_kerl['temperature_K'] = temperature_K
 
     for k in keys:
         dict_kerl[k] = np.zeros(np.shape(temperature_K)[0])
         for i, val in enumerate(temperature_K):
             dict_kerl[k][i] = optics.kerl_polarizability_temperature(val, k,
                                                                 wavelength_nm)
+    if not fig_config:
+        return dict_kerl
 
     if fig_config:
         fig = plt.figure(figsize=(fig_config['fig_width'],
                                       fig_config['fig_height']))
         for k in keys:
             plt.plot(temperature_K, dict_kerl[k],
-                    linewidth=fig_config['line_width'], label=k)
+                    linewidth=fig_config['line_width'])
 
-        plt.xlabel('Temperature $[K]$',
-                    fontsize=fig_config['axis_label_size'])
-        plt.ylabel('Polarizability $[m^3]$',
-                   fontsize=fig_config['axis_label_size'])
+            plt.xlabel('Temperature $[K]$',
+                        fontsize=fig_config['axis_label_size'])
+            plt.ylabel('Polarizability $[m^3]$',
+                       fontsize=fig_config['axis_label_size'])
 
-        ## Maybe Move this Out ##
-        plt.legend(fontsize=fig_config['legend_size'])
-        plt.xticks(fontsize=fig_config['ticks_size'])
-        plt.yticks(fontsize=fig_config['ticks_size'])
+            ## Maybe Move this Out ##
+            plt.xticks(fontsize=fig_config['ticks_size'])
+            plt.yticks(fontsize=fig_config['ticks_size'])
 
-        if output_png is not None:
-            plt.savefig(os.path.join(output_png,
-            f'kerlPolarizability_{wavelength_nm}nm.png'), format = 'png',
+            if output_png is not None:
+                plt.savefig(os.path.join(output_png,
+                f'kerlPolarizability_{k}_{wavelength_nm}nm.png'),
+                        format = 'png',
+                        bbox_inches='tight', dpi=fig_config['dpi_size'])
+                plt.close()
+            if output_png is None:
+                plt.show()
+
+def test_buldakov_method(fig_config=None, output_png=None):
+    molecule = ['H2', 'N2', 'O2']
+    temperature_K = np.arange(100, 1500 + 50, 50)
+    vibrational_number = 1 
+    rotational_number = 2
+    vib_axis = range(vibrational_number + 1)
+    rot_axis = range(rotational_number + 1)
+    buldakov_dict = { }
+    buldakov_dict['temperature_K'] = temperature_K
+
+    for k in molecule:
+        buldakov = np.zeros([np.size(temperature_K)])
+        expansion = np.zeros([vibrational_number + 1,
+                            rotational_number + 1])
+        distribution = np.zeros([np.size(temperature_K),
+                                 vibrational_number + 1,
+                                 rotational_number + 1])
+        for j in rot_axis:
+            for v in vib_axis:
+                expansion[v,j] = optics.buldakov_expansion(
+                                vibrational_number=v,
+                                rotational_number=j,
+                                molecule=k)
+
+        # Probability Distribution
+        for t, val in enumerate(temperature_K):
+            distribution[t] = quantum_mechanics.boltzmann_distribution( 
+                                                val, k, 
+                                                vibrational_number,
+                                                rotational_number, False)
+
+            buldakov[t] = np.sum(distribution[t] * expansion)
+
+        buldakov_dict[k] = buldakov
+
+    if not fig_config:
+        return buldakov_dict
+        
+    if fig_config:
+        for k in molecule:
+            fig = plt.figure(figsize=(fig_config['fig_width'],
+                                      fig_config['fig_height']))
+            plt.plot(temperature_K, buldakov_dict[k],
+                    linewidth=fig_config['line_width'])
+
+            plt.xlabel('Temperature $[K]$',
+                        fontsize=fig_config['axis_label_size'])
+            plt.ylabel('Polarizability $[m^3]$',
+                       fontsize=fig_config['axis_label_size'])
+
+            ## Maybe Move this Out ##
+            #plt.legend(fontsize=fig_config['legend_size'])
+            plt.xticks(fontsize=fig_config['ticks_size'])
+            plt.yticks(fontsize=fig_config['ticks_size'])
+
+            if output_png:
+                plt.savefig(os.path.join(output_png,
+                    f'buldakovPolarizability_{k}.png'), format = 'png',
                     bbox_inches='tight', dpi=fig_config['dpi_size'])
-            plt.close()
-        if output_png is None:
-            plt.show()
+                plt.close()
+            else:
+                plt.show()
 
 def test_quantum_mechanics_module(fig_config=None, output_png=None):
     vibrational_number = 1 
@@ -102,6 +168,8 @@ def test_quantum_mechanics_module(fig_config=None, output_png=None):
     molecule = ['N2', 'O2', 'NO']
     elev_ang = 16
     azim_ang = 40
+    vib_axis = range(vibrational_number + 1)
+    rot_axis = range(rotational_number + 1)
 
     for k in molecule:
         # [temperature, vibrational, rotational]
@@ -113,68 +181,101 @@ def test_quantum_mechanics_module(fig_config=None, output_png=None):
                                                 val, k, 
                                                 vibrational_number,
                                                 rotational_number, False)
-            if fig_config:
-                # Plots Distribution function
-                vib_axis = range(vibrational_number + 1)
-                rot_axis = range(rotational_number + 1)
-                vib_mesh, rot_mesh = np.meshgrid(vib_axis, rot_axis)
-                fig, ax = plt.subplots(subplot_kw={"projection": "3d"},
-                                       figsize=(fig_config['fig_width'],
-                                                fig_config['fig_height']))
-                # Color options 
-                colormaps = ['viridis', 'plasma', 'cividis']
+        if fig_config:
+            # Plots Distribution function
+            vib_mesh, rot_mesh = np.meshgrid(vib_axis, rot_axis)
+            fig, ax = plt.subplots(subplot_kw={"projection": "3d"},
+                                   figsize=(fig_config['fig_width'],
+                                            fig_config['fig_height']))
+            # Color options 
+            colormaps = ['viridis', 'plasma', 'cividis']
 
-                # Plot tempeteratures
-                for i, temp in enumerate(temperature_K):
-                    cmap = plt.get_cmap(colormaps[i])
-                    surf = ax.plot_surface(vib_mesh.T, rot_mesh.T, 
-                                           distribution[i],
-                                           cmap=cmap, edgecolor='k',
-                                           alpha=0.8,
-                                           linewidth=0.5)
-                    ax.text(x=vibrational_number // 2, 
-                            y=rotational_number // (4 - i),
-                            z=np.max(distribution[i]),
-                            s=f'T = {temp} [K]', color='k', 
-                            fontsize=fig_config['legend_size'])
+            # Plot tempeteratures
+            for i, temp in enumerate(temperature_K):
+                cmap = plt.get_cmap(colormaps[i])
+                surf = ax.plot_surface(vib_mesh.T, rot_mesh.T, 
+                                       distribution[i],
+                                       cmap=cmap, edgecolor='k',
+                                       alpha=0.8,
+                                       linewidth=0.5)
+                ax.text(x=vibrational_number // 2, 
+                        y=rotational_number // (4 - i),
+                        z=np.max(distribution[i]),
+                        s=f'T = {temp} [K]', color='k', 
+                        fontsize=fig_config['legend_size'])
 
-                # Change grid and background colors
-                ax.set_facecolor('white') 
-                fig.patch.set_facecolor('white')
-                ax.grid(False)
-                ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0)) 
-                ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
-                ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+            # Change grid and background colors
+            ax.set_facecolor('white') 
+            fig.patch.set_facecolor('white')
+            ax.grid(False)
+            ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 1.0)) 
+            ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
+            ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
 
-                # 3D axis views
-                ax.view_init(elev=elev_ang, azim=azim_ang)
+            # 3D axis views
+            ax.view_init(elev=elev_ang, azim=azim_ang)
 
-                # Labels
-                ax.set_xlabel('Vibrational # $[\;]$',
-                               fontsize=fig_config['axis_label_size'])
+            # Labels
+            ax.set_xlabel('Vibrational # $[\;]$',
+                           fontsize=fig_config['axis_label_size'])
 
-                ax.set_ylabel('Rotational # $[\;]$',
-                               fontsize=fig_config['axis_label_size'])
+            ax.set_ylabel('Rotational # $[\;]$',
+                           fontsize=fig_config['axis_label_size'])
 
-                ax.set_zlabel('Boltzmann Distribution $[\;]$',
-                               fontsize=fig_config['axis_label_size'])
+            ax.set_zlabel('Boltzmann Distribution $[\;]$',
+                           fontsize=fig_config['axis_label_size'])
 
-                # Integers for axis ticks
-                ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-                ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+            # Integers for axis ticks
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
 
-                if output_png:
-                    plt.savefig(os.path.join(output_png,
-                                f'boltzmannDistribution_{k}.png'), 
-                                format = 'png',
-                            bbox_inches='tight', 
-                            pad_inches=0.3,
-                            dpi=fig_config['dpi_size'])
-                    plt.close()
-                else:
-                    plt.show()
+            if output_png:
+                plt.savefig(os.path.join(output_png,
+                            f'boltzmannDistribution_{k}.png'), 
+                            format = 'png',
+                        bbox_inches='tight', 
+                        pad_inches=0.3,
+                        dpi=fig_config['dpi_size'])
+                plt.close()
+            else:
+                plt.show()
             
+def plot_buldakov_kerl(fig_config, output_png):
+    buldakov = test_buldakov_method()
+    kerl = test_optics_module()
+    keys = ['N2', 'O2', 'H2']
+
+    for k in keys:
+        fig = plt.figure(figsize=(fig_config['fig_width'],
+                                  fig_config['fig_height']))
+        plt.plot(kerl['temperature_K'], kerl[k],
+                linewidth=fig_config['line_width'], 
+                label='Kerl')
+
+        plt.plot(buldakov['temperature_K'], buldakov[k],
+                linewidth=fig_config['line_width'], 
+                label='Buldakov')
+
+        plt.xlabel('Temperature $[K]$',
+                    fontsize=fig_config['axis_label_size'])
+        plt.ylabel('Polarizability $[m^3]$',
+                   fontsize=fig_config['axis_label_size'])
+
+        ## Maybe Move this Out ##
+        plt.xticks(fontsize=fig_config['ticks_size'])
+        plt.yticks(fontsize=fig_config['ticks_size'])
+        plt.legend(fontsize=fig_config['legend_size'])
+
+
+        if output_png:
+            plt.savefig(os.path.join(output_png,
+                f'polarizabilityComparison_{k}.png'), format = 'png',
+                bbox_inches='tight', dpi=fig_config['dpi_size'])
+            plt.close()
+        else:
+            plt.show()
+
 
 if __name__ == "__main__":
     fig_config = {}
@@ -190,13 +291,10 @@ if __name__ == "__main__":
     output_png = "../figures/chapter4"
 
     test_aerodynamics_module(fig_config, output_png)
-
     test_optics_module(fig_config, output_png)
-
     test_quantum_mechanics_module(fig_config, output_png) 
-
-
-
+    test_buldakov_method(fig_config, output_png)
+    plot_buldakov_kerl(fig_config, output_png)
 
 
 
