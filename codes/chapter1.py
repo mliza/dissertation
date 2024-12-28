53b5f1e4d5bf40255fc4d2c8c5a58711dcda1699
Martin E. Liza <mliza@email.arizona.edu>
2024-12-28 09:03:45 -0500

    Updated chapter1 and chapter2 python scripts

--- codes/chapter1.py
+++ codes/chapter1.py
@@ -5,6 +5,7 @@
     Def:    Plots chapter 1
 
 """
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,12 +14,19 @@ from ambiance import Atmosphere
 
 
 def plot_index(blue_sky, altitude, output_png_path, fig_config):
+    """
+    Calculates and plot the index of refraction as a function of
+    altitude for a blue-sky case, using the optics module in the HAOT package
+    """
+
+    # Using Atmosphere to get the layers
     atm = Atmosphere(altitude)
     layers = atm.layer_name
     layer_index = np.unique(layers, return_index=True)
     color_layers = "mediumorchid"
     color_atm = "black"
 
+    # Plots altitude vs. Index of refraction
     plt.plot(
         blue_sky - 1,
         altitude * 1e-3,
@@ -27,7 +35,8 @@ def plot_index(blue_sky, altitude, output_png_path, fig_config):
         color=color_atm,
     )
 
-    for k, val in enumerate(layer_index[0]):
+    # Plot atmospheric layers
+    for k, layer in enumerate(layer_index[0]):
         plt.axhline(
             y=altitude[layer_index[1][k]] * 1e-3, linestyle="--", color=color_layers
         )
@@ -35,15 +44,15 @@ def plot_index(blue_sky, altitude, output_png_path, fig_config):
         x_min, x_max = plt.xlim()
         x_pos_left = x_min + 0.05 * (
             x_max - x_min
-        )  # Set text position as 5% from the left
+        )  # Set text position as 5% from the right
         x_pos_right = x_max - 0.05 * (
             x_max - x_min
         )  # Set text position as 5% from the left
-        if val == layer_index[0][-1]:
+        if layer == layer_index[0][-1]:
             plt.text(
-                x=x_pos_left,  # [x_pos_left, x_pos_right]
+                x=x_pos_left,
                 y=altitude[layer_index[1][k]] * 1e-3,  # Altitude for the text
-                s=val,  # Layer name
+                s=layer,
                 color=color_layers,
                 fontsize=10,
                 ha="left",  # [right,left]
@@ -51,19 +60,21 @@ def plot_index(blue_sky, altitude, output_png_path, fig_config):
             )
         else:
             plt.text(
-                x=x_pos_right,  # [x_pos_left, x_pos_right]
+                x=x_pos_right,
                 y=altitude[layer_index[1][k]] * 1e-3,  # Altitude for the text
-                s=val,  # Layer name
+                s=layer,
                 color=color_layers,
                 fontsize=10,
                 ha="right",  # [right,left]
                 va="bottom",  # Align text below the line
             )
 
+    # X and Y axis
     plt.xlabel("(Refractive Index - 1) $[\;]$", fontsize=fig_config["axis_label_size"])
     plt.ylabel("Altitude $[km]$", fontsize=fig_config["axis_label_size"])
     # plt.legend(fontsize=fig_config['legend_size'])
 
+    # Save figure
     plt.savefig(
         os.path.join(output_png_path, "atmosphericOptics.pdf"),
         format="pdf",
--- codes/chapter2.py
+++ codes/chapter2.py
@@ -5,20 +5,70 @@
     Def:    Plots chapter 2
 
 """
-import matplotlib.pyplot as plt
+
 from ambiance import Atmosphere
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
 
-def temperature_ratio_plot(data_path, plotting_path):
+def temperature_ratio_plot(data_path, plotting_path, fig_config):
+    """
+    Plots temperature ratio and compare results between calorically
+    perfect gas and nonequilibrium gas
+    """
+
+    # Loading csv data
     perfect_gas = pd.read_csv(os.path.join(data_path, "calorically_perfect.csv"))
     e1 = pd.read_csv(os.path.join(data_path, "1e-1atm.csv"))
     e2 = pd.read_csv(os.path.join(data_path, "1e-2atm.csv"))
     e4 = pd.read_csv(os.path.join(data_path, "1e-4atm.csv"))
 
-    # Config Figure
+    # Plotting
+    plt.plot(
+        perfect_gas["U"],
+        perfect_gas["T_ratio"],
+        label="Calorically perfect gas",
+        linewidth=fig_config["line_width"],
+    )
+
+    plt.plot(
+        e1["U"],
+        e1["T_ratio"],
+        label="$1e^{-1}\,[atm],\;h=16[km]$",
+        linewidth=fig_config["line_width"],
+    )
+
+    plt.plot(
+        e2["U"],
+        e2["T_ratio"],
+        label="$1e^{-2}\,[atm],\;h=31[km]$",
+        linewidth=fig_config["line_width"],
+    )
+
+    plt.plot(
+        e4["U"],
+        e4["T_ratio"],
+        label="$1e^{-4}\,[atm],\;h=65[km]$",
+        linewidth=fig_config["line_width"],
+    )
+
+    plt.legend(fontsize=fig_config["legend_size"])
+    plt.xlabel("$U_1\,[km/s]$", fontsize=fig_config["axis_label_size"])
+    plt.ylabel("$T_2/T_1\,[\;]$", fontsize=fig_config["axis_label_size"])
+    plt.savefig(
+        os.path.join(ploting_path, "temperatureRatio.pdf"),
+        format="pdf",
+        bbox_inches="tight",
+        dpi=600,
+    )
+    plt.close()
+
+
+if __name__ == "__main__":
+    ploting_path = "../figures/chapter2"
+
+    # Figure configuration
     fig_config = {}
     fig_config["line_width"] = 3
     fig_config["fig_width"] = 6
@@ -29,35 +79,4 @@ def temperature_ratio_plot(data_path, plotting_path):
     fig_config["axis_label_size"] = 16
     fig_config["title_size"] = 18
 
-    # Plotting
-    plt.plot(
-        perfect_gas["U"],
-        perfect_gas["T_ratio"],
-        label="Calorically perfect gas",
-        linewidth=fig_config['line_width']
-    )
-
-    plt.plot(
-        e1["U"], e1["T_ratio"], label="$1e^{-1}\,[atm],\;h=16[km]$",
-        linewidth=fig_config['line_width']
-    )
-
-    plt.plot(e2["U"], e2["T_ratio"], label="$1e^{-2}\,[atm],\;h=31[km]$", 
-        linewidth=fig_config['line_width'])
-
-
-    plt.plot(
-        e4["U"], e4["T_ratio"], label="$1e^{-4}\,[atm],\;h=65[km]$",
-        linewidth=fig_config['line_width'])
-
-    plt.legend(fontsize=fig_config['legend_size'])
-    plt.xlabel("$U_1\,[km/s]$", fontsize=fig_config["axis_label_size"])
-    plt.ylabel("$T_2/T_1\,[\;]$", fontsize=fig_config["axis_label_size"])
-    plt.savefig(os.path.join(ploting_path, "temperatureRatio.pdf"),
-                format="pdf", bbox_inches="tight", dpi=600)
-    plt.close()
-
-
-if __name__ == "__main__":
-    ploting_path = "../figures/chapter2"
-    temperature_ratio_plot("calloricallyPerfectGasData", ploting_path)
+    temperature_ratio_plot("calloricallyPerfectGasData", ploting_path, fig_config)
