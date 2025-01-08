9e20b82ed33c8af1e78ccdaac3754b63c2692315
Martin E. Liza <mliza@email.arizona.edu>
2025-01-07 20:12:08 -0500

    Updated figures and added all AO results

--- codes/chapter5_3.py
+++ codes/chapter5_3.py
@@ -2,6 +2,7 @@ import pickle
 import haot
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+import scipy.constants as s_const
 import IPython
 import os
 
@@ -139,7 +140,7 @@ def plot_stagnation_fields(data_in, fig_config, out_pdf_fig, cut_dict=None):
 
         if cut_dict and 'temperature' in cut_dict[n]:
             plt.xlim(cut_dict[n]['temperature'])
-        plt.savefig(os.path.join(out_pdf_fig, f'{n}_indexOfRefraction.pdf'),
+        plt.savefig(os.path.join(out_pdf_fig, f'{n}_totalGD.pdf'),
                     format='pdf', bbox_inches='tight',
                     dpi=fig_config['dpi_size'])
         plt.close() 
@@ -162,11 +163,36 @@ def plot_stagnation_fields(data_in, fig_config, out_pdf_fig, cut_dict=None):
 
         if cut_dict and 'temperature' in cut_dict[n]:
             plt.xlim(cut_dict[n]['temperature'])
-        plt.savefig(os.path.join(out_pdf_fig, f'{n}_totalGD.pdf'),
+        plt.savefig(os.path.join(out_pdf_fig, f'{n}_indexOfRefraction.pdf'),
                     format='pdf', bbox_inches='tight',
                     dpi=fig_config['dpi_size'])
         plt.close() 
         ## Plot Index of Refraction ##
+
+        ## Plot Dielectric ## 
+        plt.plot(noneq['x'] * 1E3, noneq_dielectric['dilute'] /
+                 s_const.epsilon_0, color=colors[0],
+            linewidth=fig_config['line_width'], label='Nonequilibrium')
+
+        plt.plot(frozen['x'] * 1E3, frozen_dielectric['dilute'] /
+                 s_const.epsilon_0,
+                 color=colors[1],
+                 linewidth=fig_config['line_width'], label='Frozen')
+
+        plt.xlabel('X $[mm]$', fontsize=fig_config['axis_label_size'])
+        plt.ylabel('$ \\epsilon_m / \\epsilon_0$ $[ ]$', fontsize=fig_config['axis_label_size'])
+
+        plt.xticks(fontsize=fig_config['ticks_size'])
+        plt.yticks(fontsize=fig_config['ticks_size'])
+        plt.legend(fontsize=fig_config['legend_size'])
+
+        if cut_dict and 'temperature' in cut_dict[n]:
+            plt.xlim(cut_dict[n]['temperature'])
+        plt.savefig(os.path.join(out_pdf_fig, f'{n}_dielectricMedium.pdf'),
+                    format='pdf', bbox_inches='tight',
+                    dpi=fig_config['dpi_size'])
+        plt.close() 
+        ## Plot Dielectric ## 
 
 def create_mass_density_dict(data_in, species):
     dict_out = { }
