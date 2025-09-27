9e19be77163e98a5cd7a1824fc35ab0aaf7741c2
Martin E. Liza <mliza@email.arizona.edu>
2025-09-27 13:20:23 -0400

    updarted fituiers

--- codes/index.py
+++ codes/index.py
@@ -8,11 +8,12 @@ import IPython
 
 
 # User Inputs #
-altitude = np.linspace(0, 81e3, 1000) #(0 to 81) [km]
-mach_in = np.linspace(0.4, 25, 1000) #(0.4 to 10) [mach]
-wavelength_nm = 633 
+altitude = np.linspace(0, 81e3, 80) #(0 to 81) [km]
+mach_in = np.linspace(1, 10, 80) #(0.4 to 10) [mach]
+wavelength_nm = 1000 
 
-atm = Atmosphere(altitude)
+#atm = Atmosphere(altitude)
+atm = Atmosphere(10E3)
 normal_relations = haot.normal_shock_relations(mach_in)
 density = atm.density
 temperature = atm.temperature
@@ -24,17 +25,19 @@ index_post_shock = haot.index_of_refraction_density_temperature(temperature_2,
                                                                 density_2,
                                                                 "Air",
                                                                 wavelength_nm)
+
 fig_config = figure_configurations.figure_settings()
 fig, ax1 = plt.subplots(figsize=(fig_config["fig_width"] + 2, fig_config["fig_height"]))
-ax1.semilogy(mach_2, index_post_shock['dilute'] - 1,
+ax1.semilogy(mach_in, index_post_shock['dilute'] - 1,
              linewidth=fig_config["line_width"], label='Dilute')
-ax1.semilogy(mach_2, index_post_shock['dilute'] - 1,
+ax1.semilogy(mach_in, index_post_shock['dense'] - 1,
              linewidth=fig_config["line_width"], label='Dense')
 
 ax1.set_xlabel('Mach $[ ]$', fontsize=fig_config["axis_label_size"])
 ax1.set_ylabel('n - 1 $[ ]$', fontsize=fig_config["axis_label_size"])
 ax1.legend(fontsize=fig_config["legend_size"])
 
+"""
 ax2 = ax1.twiny()
 ax2.set_xlim(ax1.get_xlim())
 
@@ -42,9 +45,10 @@ ax2.set_xlim(ax1.get_xlim())
 temp_ticks = ax1.get_xticks()
 ax2.set_xticks(temp_ticks)
 ax2.set_xticklabels([f"{rho:.4f}" for rho in np.interp(temp_ticks,
-                                                       altitude, density_2)])
+                                                       altitude, mach_2)])
 
 ax2.set_xlabel(r'$\rho$ [kg/m$^3$]', fontsize=fig_config["axis_label_size"])
+"""
 plt.savefig(f'dilute_dense_mach_{wavelength_nm}.pdf', format="pdf", bbox_inches="tight",
             dpi=fig_config["dpi_size"])
 plt.close()
@@ -54,7 +58,7 @@ fig_config = figure_configurations.figure_settings()
 fig, ax1 = plt.subplots(figsize=(fig_config["fig_width"] + 2, fig_config["fig_height"]))
 ax1.semilogy(altitude, index_post_shock['dilute'] - 1,
              linewidth=fig_config["line_width"], label='Dilute')
-ax1.semilogy(altitude, index_post_shock['dilute'] - 1,
+ax1.semilogy(altitude, index_post_shock['dense'] - 1,
              linewidth=fig_config["line_width"], label='Dense')
 
 ax1.set_xlabel('Altitude $[m]$', fontsize=fig_config["axis_label_size"])
